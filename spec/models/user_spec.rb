358c1f18cac3ef796837be561ef160b7ba176b0c
Michael Orr <michaelhyorr@gmail.com>
2018-02-20 09:10:10 -0800

    sgoco

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @user = User.create(email:  "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
     end
 
     it "can be created" do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
 
     it "cannot be created without first_name, last_name" do
       @user.first_name =  nil
-      @user.first_name =  nil
+      @user.last_name =  nil
       expect(@user).to_not be_valid
     end
   end
