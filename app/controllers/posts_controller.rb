8412475949126977afe2b5328096541e0eb4833e
Michael Orr <michaelhyorr@gmail.com>
2018-02-20 11:01:02 -0800

    add

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,32 @@
 class PostsController < ApplicationController
-    def index
-    end
+	before_action :set_post, only: [:show]
+	def index
+	end
 
-    def new
-        @post = Post.new
-    end
+	def new
+		@post = Post.new
+	end
 
-    def create
-        @post = Post.new(params.require(:post).permit(:date, :rationale))
+	def create
+		@post = Post.new(post_params)
 
-        @post.save
+		if @post.save
+			redirect_to @post, notice: 'Your post was created successfully'
+		else
+			render :new
+		end
+	end
 
-        redirect_to @post
-    end
+	def show
+	end
 
-    def show
-        @post = Post.find(params[:id])
-    end
+	private
+
+	  def post_params
+	  	params.require(:post).permit(:date, :rationale)
+	  end
+
+	  def set_post
+	  	@post = Post.find(params[:id])
+	  end
 end
